cd7e53e51397dbf6d752f01e8789ef8b71bbdbf5
Teddy Gonyea <enterted@gmail.com>
2025-10-14 15:08:25 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,50 +1,88 @@
+-- Leader key
 vim.g.mapleader = " "
 
+-- Basic options
 vim.opt.number = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.signcolumn = "yes"
 vim.opt.winborder = "rounded"
-
-vim.opt.clipboard = 'unnamedplus'
-
+vim.opt.clipboard = "unnamedplus"
 vim.opt.swapfile = false
 
-vim.pack.add({
-    { src = "https://github.com/vague2k/vague.nvim" },
-    { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
-    { src = "https://github.com/nvim-lua/plenary.nvim" },
-    { src = "https://github.com/nvim-telescope/telescope.nvim", tag = "0.1.x" },
-    { src = 'https://github.com/neovim/nvim-lspconfig' },
+-- Bootstrap lazy.nvim if not installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugin setup
+require("lazy").setup({
+  -- Theme
+  {
+    "vague2k/vague.nvim",
+    config = function()
+      require("vague").setup({ transparent = true })
+      vim.cmd("colorscheme vague")
+      vim.cmd("hi Normal guibg=NONE")
+      vim.cmd("hi StatusLine guibg=NONE")
+    end,
+  },
+
+  -- File explorer
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    keys = {
+      { "<leader>e", "<cmd>Oil<CR>", desc = "Open Oil file explorer" },
+    },
+  },
+
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = { "lua", "c", "markdown", "markdown_inline" },
+      highlight = { enable = true },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+
+  -- Telescope (with dependency)
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({})
+      local map = vim.keymap.set
+      map("n", "<leader>f", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
+      map("n", "<leader>h", "<cmd>Telescope help_tags<CR>", { desc = "Help tags" })
+    end,
+  },
+
+  -- LSP Config
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({})
+      lspconfig.lemminx.setup({})
+      vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "Format file" })
+    end,
+  },
 })
-
-require"telescope".setup{}
-require "oil".setup()
-
-local map = vim.keymap.set
-map('n', '<leader>f', ":Telescope find_files<CR>")
-map('n', '<leader>h', ":Telescope help_tags<CR>")
-map('n', '<leader>e', ":Oil<CR>")
-map('n', '<leader>lf', vim.lsp.buf.format)
-
-local lspconfig = require('lspconfig')
-
-lspconfig.lua_ls.setup({})
-lspconfig.clangd.setup({})
-lspconfig.lemminx.setup({})
-
--- Treesitter
-vim.cmd.packadd("nvim-treesitter")
-require('nvim-treesitter.configs').setup {
-    ensure_installed = { "lua", "c", "markdown", "markdown_inline" },
-    highlight = { enable = true },
-}
-
--- colorscheme
-require "vague".setup({ transparent = true })
-vim.cmd("colorscheme vague")
-vim.cmd("hi Normal guibg=NONE")
-vim.cmd(":hi statusline guibg=NONE")
 
