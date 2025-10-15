e24a3787bcadc693437b2ff9c3db79edb94e952a
Teddy Gonyea <enterted@gmail.com>
2025-10-15 18:12:35 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -70,7 +70,7 @@ require("lazy").setup({
   -- Telescope
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.x",
+    tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({})
