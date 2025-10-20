dec9f06fe7cd01093d63f3ffc58eca27ea66917b
Teddy Gonyea <enterted@gmail.com>
2025-10-20 14:44:19 -0400

    Update .config/polybar/launch.sh

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-# NOTE: The command 'killall -q polybar' is the "nuclear" option.
-# If you have IPC enabled, 'polybar-msg cmd quit' is cleaner.
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
-
-# Launch the bar(s)
-# Replace 'example' with the name of the bar defined in your config.ini (e.g., [bar/top])
-# If you have multiple bars/monitors, you may need a loop here (see ArchWiki for multi-monitor setup).
-polybar main &
+pkill polybar
+polybar &
 
 echo "Polybar launched..."
