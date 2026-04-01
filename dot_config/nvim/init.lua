90f8219045aa474ad169eb25ee126f67f3655edc
Teddy Gonyea <enterted@gmail.com>
2026-03-31 22:37:24 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -38,6 +38,22 @@ require("lazy").setup({
         end,
     },
 
+    {
+        'projekt0n/github-nvim-theme',
+        name = 'github-theme',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('github-theme').setup({
+                options = {
+                    transparent = true,
+                }
+            })
+
+            vim.cmd('colorscheme github_dark_default')
+        end,
+    }
+
     -- File explorer
     {
         "stevearc/oil.nvim",
