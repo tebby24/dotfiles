8b002241db51fa16b0b7f5ebe546d149bddbfc8c
Teddy Gonyea <enterted@gmail.com>
2026-07-07 21:34:40 -0400

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,10 +1,15 @@
 vim.cmd.packadd('nohlsearch')
 
 vim.pack.add({
-  'https://github.com/neovim/nvim-lspconfig',
-  'https://github.com/ibhagwan/fzf-lua',
-  'https://github.com/nvim-mini/mini.completion',
+    'https://github.com/vague-theme/vague.nvim',
+    'https://github.com/neovim/nvim-lspconfig',
+    'https://github.com/ibhagwan/fzf-lua',
+    'https://github.com/nvim-mini/mini.completion',
 })
 
+vim.cmd.colorscheme('vague')
+require('vague').setup { transparent = true }
+
 require('fzf-lua').setup { fzf_colors = true }
+
 require('mini.completion').setup {}
