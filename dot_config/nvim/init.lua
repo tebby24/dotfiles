bd0100f5ac8dbea95e901ceafc45fa449e7fa938
Teddy Gonyea <enterted@gmail.com>
2026-09-16 23:58:27 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -32,6 +32,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- plugins
 vim.cmd.packadd('nohlsearch')
 vim.pack.add({
+    'https://github.com/nvim-treesitter/nvim-treesitter',
     'https://github.com/vague-theme/vague.nvim',
     'https://github.com/neovim/nvim-lspconfig',
     'https://github.com/ibhagwan/fzf-lua',
@@ -39,6 +40,18 @@ vim.pack.add({
     'https://github.com/m4xshen/autoclose.nvim',
     'https://github.com/nvim-mini/mini.surround',
 })
+
+require{'nvim-treesitter.configs'}.setup {
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<CR>",     -- start incremental selection
+            node_incremental = "<TAB>",  -- increment to upper named node
+            scope_incremental = "<CR>",  -- increment to current scope
+            node_decremental = "<S-TAB>",-- decrement to previous node
+        },
+    },
+}
 
 require('vague').setup { transparent = true }
 vim.cmd.colorscheme('vague')
